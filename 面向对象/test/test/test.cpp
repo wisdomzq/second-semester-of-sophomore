73991b3584780695c06832d26ac01b922a531b8d
Li Zequn <1120231246@bit.edu.cn>
2025-02-26 10:52:55 +0800

    1

--- 面向对象/test/test/test.cpp
+++ 面向对象/test/test/test.cpp
@@ -1,6 +1,27 @@
-#include<iostream>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include<vector>
 using namespace std;
-int main()
+int main() 
 {
-	cout << "Hello world" << endl;
+    
+    ofstream out1("File1.txt");
+	ofstream out2("File2.txt");
+	vector<string> v;
+    string s;
+    while (cin >> s)
+	{
+		v.push_back(s);
+    }
+	for (int i = 0; i < v.size(); i++)
+		out1 << v[i] << endl;
+	out1.close();
+	ifstream in1("File1.txt");
+	while (getline(in1, s))
+	{
+		out2 << s << endl;
+		cout << s << endl;
+	}
+    return 0;
 }
